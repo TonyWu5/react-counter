2a1e785ee07625c9b597fbf4c1f9dd63f66cb616
Tony <tnywu@ucdavis.edu>
2019-05-05 05:08:17 -0700

    set up webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const SRC_DIR = path.join(__dirname, '/client/src');
+const DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  mode: 'development',
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            '@babel/preset-env',
+            '@babel/preset-react'
+          ]
+        }
+      }
+    ]
+  }
+};
